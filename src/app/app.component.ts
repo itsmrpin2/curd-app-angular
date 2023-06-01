a5439a1fab3f7846ccf9d8796d2bcea28160c2cd
Chitta Nanda <ranjanchitta.nanda@gmail.com>
2023-06-02 03:53:01 +0530

    curd app done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,90 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { EmpAddEditComponent } from './emp-add-edit/emp-add-edit.component';
+import { EmployeeService } from './services/employee.service';
+import {MatPaginator, MatPaginatorModule} from '@angular/material/paginator';
+import {MatSort, MatSortModule} from '@angular/material/sort';
+import {MatTableDataSource, MatTableModule} from '@angular/material/table';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { CoreService } from './core/core.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'curd-app';
+  displayedColumns: string[] = ['id', 'firstName', 'lastName', 'email','dob','gender','education','company','experience','package','action'];
+  dataSource!: MatTableDataSource<any>;
 
-  constructor(private _dialog: MatDialog){}
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
 
-  openAddEditEmpForm(){
-    this._dialog.open(EmpAddEditComponent)
+  constructor(
+    private _matDialog: MatDialog,
+     private _empService: EmployeeService,
+     private _coreService: CoreService,
+     ){}
+
+  ngOnInit(): void {
+      this.getEmployeeList();
   }
 
+  openAddEditEmpForm(){
+    const matDialogRef = this._matDialog.open(EmpAddEditComponent);
+    matDialogRef.afterClosed().subscribe({
+      next: (val) => {
+        if(val){
+          this.getEmployeeList();
+        }
+      },
+    });
+  }
+
+  getEmployeeList(){
+    this._empService.getEmployeeList().subscribe({
+      next: (res) => {
+        console.log(res);
+        this.dataSource = new MatTableDataSource(res);
+        this.dataSource.sort = this.sort;
+        this.dataSource.paginator = this.paginator;
+      },
+      error: (err) => {
+        console.log(err);
+      }
+    })
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+  deleteEmployee(id: number) {
+    this._empService.deleteEmployee(id).subscribe({
+      next: (res) =>{
+        this._coreService.openSnackBar("Employee Deleted!",'done');
+        //alert("Employee Deleted!");
+        this.getEmployeeList();
+      },
+      error: console.log,
+    })
+  }
+    openEditForm(data: any) {
+      const matDialogRef = this._matDialog.open(EmpAddEditComponent,{
+        data,
+      });
+      matDialogRef.afterClosed().subscribe({
+        next: (val) => {
+          if(val){
+            this.getEmployeeList();
+          }
+        },
+      });
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,14 @@ import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
 import {MatDatepickerModule} from '@angular/material/datepicker';
 import { MatNativeDateModule } from '@angular/material/core';
+import {MatRadioModule} from '@angular/material/radio';
+import {MatSelectModule} from '@angular/material/select';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import {MatTableModule} from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
 
 
 @NgModule({
@@ -32,6 +40,14 @@ import { MatNativeDateModule } from '@angular/material/core';
     MatInputModule,
     MatDatepickerModule,
     MatNativeDateModule,
+    MatRadioModule,
+    MatSelectModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatSnackBarModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
